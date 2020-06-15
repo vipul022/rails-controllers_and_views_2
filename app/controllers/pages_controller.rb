a9da1e23adb5a74c8f094696525e473351ad3c10
vipul022 <vipul.seth022@gmail.com>
2020-06-15 12:11:55 +1000

    added redirect routes

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,12 +1,23 @@
 class PagesController < ApplicationController
   skip_before_action :verify_authenticity_token
+  before_action :test_before
+  after_action :test_after
     def home
       render html: "<h1> Honey I am home</h1>".html_safe
     end
 
     def data
       p params
-      render plain: params
+      redirect_to root_path
+
+    end
+
+    def test_before
+      puts "before ran"
+    end
+
+    def test_after
+      puts "after ran"
     end
 end      
      
